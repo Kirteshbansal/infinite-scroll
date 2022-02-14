aca944466d64ee9e6441da219135128cfd49417c
Kirtesh Bansal <kirtesh.bansal@siply.in>
2022-02-14 10:47:30 +0530

    Add Home Page to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./components/Layout/Header/Header";
+import Home from "./components/Pages/Home";
+
 import "./assets/css/styles.css";
 
 function App() {
     return (
         <div>
             <Header />
+            <Home />
         </div>
     );
 }
